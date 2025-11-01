55c9058837b9eb4545ba36fbefd52e1ed885c890
tutosrive <108991712+tutosrive@users.noreply.github.com>
2025-11-01 23:51:47 +0000

    Deploying to gh-pages from @ tutosrive/me@bc000b52b21b564802e558b3911fe04041b43f98 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "tutosrive/_tutosrive",
+    "data-repo": "tutosrive/me",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
